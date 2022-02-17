03bc399c8395a65efcc61a551d41ea20988d7735
thiggimajig <higgins.taylor.a@gmail.com>
2022-02-17 20:49:09 +0100

    got it working after json structure issue but should use smaller dataset and local tiles because not rendering though i see the html in the inspect dev tools overall this makes me want to stick to python because i know it better

--- react-leaflet-demo/src/App.js
+++ react-leaflet-demo/src/App.js
@@ -2,19 +2,60 @@ import React, {useState} from "react";
 import {Mapcontainer, TileLayer, Marker, Popup, MapContainer} from 'react-leaflet';
 import './App.css';
 import { Icon } from "leaflet";
-import * as strData from "./data/florence_listings_jan_27_500_2_15_2022.json"; //we'll want to use airbnb data here, csv to json, done preliminariliy at least 
-
+//import * as listingData from "./data/listingflorencelistingsjan275002162022.json"; //we'll want to use airbnb data here, we did after fixing json 
+//do i need to export something for listingData? I think it's working because I see marker data in the html i think it's just too large and potentially error is a bug in react idk saw edge case for "_" files in stackoverflow
+//not relying on 3rd party apps, reading json myself so no weird react error
+let listingData = require('./data/listingflorencelistingsjan275002162022.json');
+//console.log(listingData.listings[0].longitude);
 function App() {
+  const [activeListing, setActiveListing] = useState(null);
+  const icon = new Icon({
+    iconUrl: "../public/hotel_1.svg",
+    iconSize: [15,15]
+  });
+ 
   //return (<div> hello!</div>);
-  //make this florence coordinates - i did for sant'ambrogio
+  //make this florence coordinates, made actual florence because subset of listings may not be in sant ambrogio
   return (
-    <MapContainer center={[43.7715, 11.2666]} zoom={12}scrollWheelZoom={false}>  
+    <MapContainer center={[43.7696, 11.2558]} zoom={20}scrollWheelZoom={false}>  
       <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+    {listingData.listings.map(listing => (
+      //loop through length of json listings, the count represented by variable i, will all each element to be accessed
+      <Marker
+        key={listing.id}
+        position={[
+          listing.latitude,
+          listing.longitude
+        ]}
+        onClick = {() => {
+          setActiveListing(listing);
+        }}
+        icon={icon}
+      />
+    ))}
+    
+    {activeListing && (
+      <Popup
+        position={[
+          activeListing.latitude,
+          activeListing.longitude
+        ]}
+        onClose={() => {
+          setActiveListing(null);
+        }}
+        >
+          <div>
+            <h2>{activeListing.name}</h2>
+            <p>{activeListing.host_total_listings_count}, {activeListing.price}, {activeListing.listing_url}</p>
+          </div>
+        </Popup>
+    )}
+
     </MapContainer>
-  )
+  );
 }
 
 export default App;
--- react-leaflet-demo/src/index.js
+++ react-leaflet-demo/src/index.js
@@ -1,8 +1,19 @@
+//followed this tutorial with a bit of cleanup from the react crash course youtube video www.smashingmagazine.com/2020/02/javascript-maps-react-leaflet/  
+//important react app or component
 import React from 'react';
+//important react dom app or component
 import ReactDOM from 'react-dom';
+//for styling of the page
 import './index.css';
-import App from './App';
+//shows what will be injected into the DOM/html or main index page
+import App from './App'; 
 
-ReactDOM.render( <App />, document.getElementById('root'));
+ReactDOM.render( 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
+
 
 
